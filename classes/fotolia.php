c681a782d10aa5b5531462c9d6675cd4c105af08
emtou <mtou@charougna.com>
2012-04-10 14:24:38 +0200

    Add empty Fotolia class

---
+++ classes/fotolia.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Fotolia
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  JConfig
+ * @package   JConfig
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Fotolia
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia.php
+ */
+class Fotolia extends Fotolia_Core
+{
+}
---
+++ classes/fotolia/core.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Declares Fotolia_Core
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/core.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Fotolia_Core
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/core.php
+ */
+abstract class Fotolia_Core
+{
+
+} // End class Fotolia_Core
