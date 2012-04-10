33feb476629c76121fe94706e222bc3ec820fe96
emtou <mtou@charougna.com>
2012-04-10 14:53:19 +0200

    Add Exception handling

---
+++ classes/fotolia/core/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Fotolia_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/core/exception.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Fotolia_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/core/exception.php
+ */
+abstract class Fotolia_Core_Exception extends Kohana_Exception
+{
+} // End Fotolia_Core_Exception
---
+++ classes/fotolia/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Fotolia_Exception
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/exception.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Fotolia_Exception
+ *
+ * PHP version 5
+ *
+ * @group fotolia
+ *
+ * @category  Fotolia
+ * @package   Fotolia
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2012 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-fotolia/tree/master/classes/fotolia/exception.php
+ */
+class Fotolia_Exception extends Fotolia_Core_Exception
+{
+}
