8be9822980e23cf3afe581d551439b9596a01770
emtou <mtou@charougna.com>
2012-04-17 08:29:21 +0200

    Change API retrieving of max_items

--- classes/fotolia/core/api.php
+++ classes/fotolia/core/api.php
@@ -282,8 +282,6 @@ abstract class Fotolia_Core_API extends Fotolia
   {
     $collection = new Fotolia_Collection;
 
-    $collection->max_items = $results['nb_results'];
-
     $i = 0;
     while (array_key_exists($i, $results))
     {
@@ -300,6 +298,13 @@ abstract class Fotolia_Core_API extends Fotolia
 
       ++$i;
     }
+
+    // semihack : only believe nb_results if items are found
+    if ($i > 0)
+    {
+      $collection->max_items = $results['nb_results'];
+    }
+
     return $collection;
   }
 
